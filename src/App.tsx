119b22af6742e652ec215a37aab125e495278ab2
Rodrigo Gevaerd <rodrigo.gevaerd@compasso.com.br>
2023-12-28 16:01:27 -0300

    add some style and basic elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
+import styles from './App.module.css';
+import logo from './assets/logo.svg';
+import plusIcon from './assets/plus-icon.svg';
+import clipboard from './assets/clipboard.svg';
 
 function App() {
   return (
-    <div>
-      <h1>hello world!</h1>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img className={styles.logo} src={logo} alt="logo" />
+        <div className={styles.inputDiv}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+          <button>
+            Criar
+            <img src={plusIcon} alt="Criar" />
+          </button>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <section className={styles.infos}>
+            <span className={styles.tasksInfos}>
+              <span>
+                Tarefas Criadas
+              </span>
+              <div className={styles.counter}>
+                0
+              </div>
+            </span>
+
+            <span className={styles.tasksInfos}>
+              <span>
+                Concluidas
+              </span>
+              <div className={styles.counter}>
+                0
+              </div>
+            </span>
+
+          </section>
+          <div className={styles.line}/>
+          <section>
+            <div className={styles.noTasksMessage}>
+              <img src={clipboard} alt="clipboard" />
+              <p className={`${styles.message} ${styles.bold}`}>Você ainda não tem tarefas cadastradas</p>
+              <p className={styles.message}>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import App from './App.js'
+import App from './App.jsx'
+import './Global.module.css';
 
 ReactDOM.render(
   <React.StrictMode>
